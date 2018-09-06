0a1a114cddbc906f4e42514ee85aec9cf4bd8d3b
Greg Giannone <greg.giannone1@gmail.com>
2018-09-05 20:11:05 -0400

    Circles stored in state

--- src/droplets/core.clj
+++ src/droplets/core.clj
@@ -2,30 +2,32 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
-(defn draw-random-circle [color]
-  (q/with-fill color
-    (let [radius (q/random 10 20)
-          diameter (* 2 radius)
-          x (q/random radius (- (q/width) radius))
-          y (q/random radius (- (q/height) radius))]
-      (q/ellipse x y diameter diameter))
-  ))
+(defn draw-circle [circle]
+  (let [{radius :radius
+         x :x
+         y :y
+         color :color} circle]
+    (q/with-fill color
+        (q/ellipse x y (* 2 radius) (* 2 radius)))))
 
 (defn setup []
   (q/no-stroke)
   (q/frame-rate 60)
   (q/color-mode :hsb)
-  (q/background 240))
+  (q/background 240)
+  {:circles [{:radius 50 :x 100 :y 100 :color [220 180 180]}]})
 
-;(defn update)
+(defn update [state]
+  (update-in state [:radius] inc))
 
-(defn draw []
-  (draw-random-circle [(q/random 200 225) (q/random 100 200) (q/random 100 200)]))
+(defn draw [state]
+  (draw-circle (get (get state :circles) 0)))
 
 (q/defsketch droplets
   :title "Droplets"
   :size [500 500]
   :setup setup
-  ;:update update
+  :update update
   :draw draw
-  :features [:keep-on-top])
+  :features [:keep-on-top]
+  :middleware [m/fun-mode])
