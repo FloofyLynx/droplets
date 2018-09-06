2de4bfc7c475f7cf04b3ab597667a98dc618904d
Greg Giannone <greg.giannone1@gmail.com>
2018-09-05 20:38:26 -0400

    Ever expanding circle

--- src/droplets/core.clj
+++ src/droplets/core.clj
@@ -2,6 +2,11 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
+(defn -main [& args]
+  (println ""))
+
+(def hue 51)
+
 (defn draw-circle [circle]
   (let [{radius :radius
          x :x
@@ -10,15 +15,23 @@
     (q/with-fill color
         (q/ellipse x y (* 2 radius) (* 2 radius)))))
 
+(defn create-circle []
+  (let [radius (q/random 5 10)
+        x (q/random radius (- (q/width) radius))
+        y (q/random radius (- (q/height) radius))
+        color [(q/random (- hue 20) (+ hue 20)) (q/random 180 220) (q/random 180 220)]]
+    {:radius radius :x x :y y :color color}))
+
 (defn setup []
   (q/no-stroke)
   (q/frame-rate 60)
   (q/color-mode :hsb)
   (q/background 240)
-  {:circles [{:radius 50 :x 100 :y 100 :color [220 180 180]}]})
+  {:circles [(create-circle)]})
 
-(defn update [state]
-  (update-in state [:radius] inc))
+(defn update-state [state]
+  (let [circle (get (get state :circles) 0)]
+    {:circles [(update circle :radius inc)]}))
 
 (defn draw [state]
   (draw-circle (get (get state :circles) 0)))
@@ -27,7 +40,7 @@
   :title "Droplets"
   :size [500 500]
   :setup setup
-  :update update
+  :update update-state
   :draw draw
   :features [:keep-on-top]
   :middleware [m/fun-mode])
